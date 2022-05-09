08c3ea20817d0129ce2abd4ef20f915e852c0285
NaassonTeam <66871641+NaassonTeam@users.noreply.github.com>
2022-05-09 20:42:49 +0300

    autoCommit

--- pages/test.vue
+++ pages/test.vue
@@ -16,6 +16,9 @@
 </template>
 
 <script setup>
+import { AppRow } from '#components'
+
+
 const { data, pending, error, refresh } = await useAsyncData(
 	'mountains',
 	() => $fetch('https://haton.ru/custom/t1/captions.json')
