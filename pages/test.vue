a9253652a7461a36dd5406f55d5799b82ec231e1
NaassonTeam <66871641+NaassonTeam@users.noreply.github.com>
2022-05-09 20:41:09 +0300

    autoCommit

--- pages/test.vue
+++ pages/test.vue
@@ -5,7 +5,7 @@
 
 		<div v-for="item in data" key="items" class="w-full relative grid grid-rows-1">
 
-			<app-row
+			<AppRow
 							:background-color="item.backgroundColor"
 							:margin-top="item.marginTop"
 							:margin-bottom="item.marginBottom"
