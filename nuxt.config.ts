d31a1436507ebf6e006eb718193d27c5c0ee1ef9
NaassonTeam <66871641+NaassonTeam@users.noreply.github.com>
2022-05-09 19:31:39 +0300

    Add three components Image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,63 @@
-import {defineNuxtConfig} from 'nuxt3'
+import {defineNuxtConfig} from 'nuxt'
 
 export default defineNuxtConfig({
-    nitro: {
-        preset: 'server',
-        timing: false
-    },
+    ssr: true,
+    // nitro: {
+    //     preset: 'server',
+    //     timing: false
+    // },
     // dev: true,
+
+
+    app: {
+        head: {
+            meta: [
+                // <meta name="viewport" content="width=device-width, initial-scale=1">
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+            ],
+            script: [
+                // <script src="https://myawesome-lib.js"></script>
+                { src: 'https://awesome-lib.js' }
+            ],
+            link: [
+                // <link rel="stylesheet" href="https://myawesome-lib.css">
+                { rel: 'stylesheet', href: 'https://awesome-lib.css' }
+            ],
+            // please note that this is an area that is likely to change
+            style: [
+                // <style type="text/css">:root { color: red }</style>
+                { children: ':root { color: red }', type: 'text/css' }
+            ]
+        }
+    },
+
+
     buildModules: [
         ['@pinia/nuxt']
     ],
-    build: {
-        postcss: {
-            postcssOptions: {
-                plugins: {
-                    tailwindcss: {},
-                    autoprefixer: {},
-                }
-            }
-        },
+    components: {
+        global: true,
+        dirs: ['~/components']
     },
+
+
+    // build: {
+    //     postcss: {
+    //         postcssOptions: {
+    //             plugins: {
+    //                 tailwindcss: {},
+    //                 autoprefixer: {},
+    //             }
+    //         }
+    //     },
+    // },
+
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        }
+    }
 
     /* options for vite */
     // ssr: false, // enable unstable server-side rendering for development (false by default)
