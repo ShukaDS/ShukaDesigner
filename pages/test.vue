1bc5c8e7dbea6523daa5d309d7695a4571380bb6
NaassonTeam <66871641+NaassonTeam@users.noreply.github.com>
2022-05-09 20:49:32 +0300

    autoCommit

--- pages/test.vue
+++ pages/test.vue
@@ -4,16 +4,15 @@
 		<pre>{{ items }}</pre>
 
 		<div v-for="item in items" class="w-full relative grid grid-rows-1">
+			<app-row
+							:background-color="item.backgroundColor"
+							:margin-top="item.marginTop"
+							:margin-bottom="item.marginBottom"
+							:padding-top="item.paddingTop"
+							:padding-bottom="item.paddingBottom"
+							:content="item.content"
 
-			<!--			<app-row-->
-			<!--							:background-color="item.backgroundColor"-->
-			<!--							:margin-top="item.marginTop"-->
-			<!--							:margin-bottom="item.marginBottom"-->
-			<!--							:padding-top="item.paddingTop"-->
-			<!--							:padding-bottom="item.paddingBottom"-->
-			<!--							:content="item.content"-->
-
-			<!--			/>-->
+			/>
 		</div>
 
 
