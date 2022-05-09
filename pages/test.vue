967ac89edded950fa133892c8ffb77d1654a7bb4
NaassonTeam <66871641+NaassonTeam@users.noreply.github.com>
2022-05-09 20:56:31 +0300

    autoCommit

--- pages/test.vue
+++ pages/test.vue
@@ -38,7 +38,7 @@ export default {
 
 		for(let i in listComponents.data){
 			//Object.assign({}, obj);
-			console.log(listComponents.data[i])
+			console.log(listComponents.data[i], i)
 			items.value.push(Object.assign({backgroundColor: '#000000'}, listComponents.data[i]))
 		}
 
