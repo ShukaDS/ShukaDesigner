7ed304468c6fed2d957d814d23540f7f6ca04e2b
NaassonTeam <66871641+NaassonTeam@users.noreply.github.com>
2022-06-20 01:31:21 +0300

    autoCommit

--- components/Base/Content/Default.vue
+++ components/Base/Content/Default.vue
@@ -19,7 +19,7 @@
 	<div v-if="content.type === 'vimeo'" class="relative" :style="{'padding-bottom': content.ratio + '%'}">
 
 		<client-only>
-			<vue-vimeo-player ref="player" video-id="190906129" />
+			<vue-vimeo-player ref="player" video-id="190906129" class="absolute top-0 left-0 w-full h-full" />
 		</client-only>
 
 <!--		<iframe class="absolute top-0 left-0 w-full h-full" :src="content.link"-->
